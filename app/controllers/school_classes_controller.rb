33a8b180f423b2bdf64ea190cfdb1160d890d999
Sarah Schaller <sarcsch@aol.com>
2017-11-01 17:09:10 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,17 +1,15 @@
 class SchoolClassesController < ApplicationController
-
   def show
     @school_class = SchoolClass.find(params[:id])
   end
 
   def new
-    @school_class = SchoolClass.new
   end
 
   def create
     @school_class = SchoolClass.new(school_class_params)
     @school_class.save
-    redirect_to school_class_path(@school_class_path)
+    redirect_to school_class_path(@school_class)
   end
 
   def edit
@@ -25,9 +23,8 @@ class SchoolClassesController < ApplicationController
     redirect_to school_class_path(@school_class)
   end
 
-  private
-
   def school_class_params
-    params.require(:school_class).permit(:title, :room_number)
+     params.require(:school_class).permit(:title, :room_number)
   end
+
 end
