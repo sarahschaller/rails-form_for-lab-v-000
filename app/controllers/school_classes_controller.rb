7c7cc27edfa7d6181ee6c201e90f2499dff1bf67
Sarah Schaller <sarcsch@aol.com>
2017-11-01 16:54:44 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -25,6 +25,8 @@ class SchoolClassesController < ApplicationController
     redirect_to school_class_path(@school_class)
   end
 
+  private
+
   def school_class_params
     params.require(:school_class).permit(:title, :room_number)
   end
